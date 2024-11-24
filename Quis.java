b711a06a0fe8a50cedf2fa2aa8d68b7a64c8e686
RafiShiddik <rafishiddik@gmail.com>
2024-11-24 18:53:48 +0700

    Add files via upload

---
+++ Quis.java
@@ -0,0 +1,76 @@
+//Quis
+/* Stop at This Frame
+The Animate timeline will stop/pause at the frame where you insert this code.
+Can also be used to stop/pause the timeline of movieclips.
+*/
+
+stop();
+
+/* Click to Go to Scene and Play
+Clicking on the specified symbol instance plays the movie from the specified scene and frame.
+
+Instructions:
+1. Replace "Scene 3" with the name of the scene you would like play.
+2. Replace 1 with the frame number you would like the movie to play from in the specified scene.
+*/
+
+game_1.addEventListener(MouseEvent.CLICK, fl_ClickToGoToScene_21);
+
+function fl_ClickToGoToScene_21(event:MouseEvent):void
+{
+	MovieClip(this.root).gotoAndPlay(1, "game");
+}
+
+/* Click to Go to Scene and Play
+Clicking on the specified symbol instance plays the movie from the specified scene and frame.
+
+Instructions:
+1. Replace "Scene 3" with the name of the scene you would like play.
+2. Replace 1 with the frame number you would like the movie to play from in the specified scene.
+*/
+
+home1.addEventListener(MouseEvent.CLICK, fl_ClickToGoToScene_59);
+
+function fl_ClickToGoToScene_59(event:MouseEvent):void
+{
+	MovieClip(this.root).gotoAndPlay(1, "menu_utama");
+}
+
+
+/* Click to Go to Scene and Play
+Clicking on the specified symbol instance plays the movie from the specified scene and frame.
+
+Instructions:
+1. Replace "Scene 3" with the name of the scene you would like play.
+2. Replace 1 with the frame number you would like the movie to play from in the specified scene.
+*/
+
+buku_panduan1111.addEventListener(MouseEvent.CLICK, fl_ClickToGoToScene_112);
+
+function fl_ClickToGoToScene_112(event:MouseEvent):void
+{
+	MovieClip(this.root).gotoAndPlay(67, "panduan");
+}
+//Halaman Akhir Quis
+
+/* Stop at This Frame
+The Animate timeline will stop/pause at the frame where you insert this code.
+Can also be used to stop/pause the timeline of movieclips.
+*/
+
+stop();
+nhp.text= String (nilai_1 + nilai_2 + nilai_3 + nilai_4 + nilai_5 + nilai_6 + nilai_7 + nilai_8 + nilai_91 + nilai_101);
+/* Click to Go to Scene and Play
+Clicking on the specified symbol instance plays the movie from the specified scene and frame.
+
+Instructions:
+1. Replace "Scene 3" with the name of the scene you would like play.
+2. Replace 1 with the frame number you would like the movie to play from in the specified scene.
+*/
+
+home1.addEventListener(MouseEvent.CLICK, fl_ClickToGoToScene_79);
+
+function fl_ClickToGoToScene_79(event:MouseEvent):void
+{
+	MovieClip(this.root).gotoAndPlay(1, "menu_utama");
+}
